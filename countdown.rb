9ab4dd6597228045bf8315ca33e3fa59e17ced66
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-28 16:34:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 countdown = 10 
 
-def countdown
-  while countdown < 10 
-  puts "#{countdown} SECOND(S)!."
-  countdown -= 1 
-if countdown == 0 
+def countdown(count)
+  while count < 10 
+  puts "#{count} SECOND(S)!."
+  count -= 1 
+if count == 0 
   break
 end
 end
